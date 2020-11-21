fa24cac0bb5b76b6754970189ad3968c10c9bea0
Md. Faizul Islam <faizulcse@gmail.com>
2020-11-21 15:14:44 +0600

    initial commit

---
+++ tests/basic.test.js
@@ -0,0 +1,26 @@
+import {Selector} from 'testcafe'
+
+//prettier-ignore
+fixture `Getting started with TestCafe`
+    .page('https://devexpress.github.io/testcafe/example/')
+    .before(async t => {
+        // Test setup goes here
+        // await runDatabaseReset()
+        // await seedTestData()
+    }) 
+    .beforeEach(async t => {
+        // run before each test
+    })
+    .after(async t =>{
+        // Cleaning test data
+    })
+    .afterEach(async t => {
+        // Runs after each test
+    })
+    
+test('My First testcafe test', async t =>{
+    //Here goes testcafe code
+await t.typeText('#developer-name', 'Jhon')
+await t.click('#submit-button')
+await t.expect(Selector('#article-header').innerText).contains('Jhon')
+})
