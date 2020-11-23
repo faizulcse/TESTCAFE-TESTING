4596e0593fa7358bd0adc80947639ad15c365634
fislam <fislam@rsglab.com>
2020-11-23 20:29:34 +0600

    add page element

--- page-objects/pages/FeedbackPage.js
+++ page-objects/pages/FeedbackPage.js
@@ -2,7 +2,19 @@ import { Selector, t } from 'testcafe'
 import BasePage from './BasePage'
 
 class FeedbackPage extends BasePage {
-	constructor() {}
+	constructor() {
+		this.developer_name = Selector('#developer-name')
+		this.submitButton = Selector('#submit-button')
+		this.articleText = Selector('#article-header').innerText
+	}
+
+	async setDeveloperName() {
+		await t.typeText(this.developer_name, 'Jhon')
+		await t.click(this.submitButton)
+	}
+	async getArticleText() {
+		return this.articleText.innerText
+	}
 }
 
 export default FeedbackPage
--- tests/feedback.test.js
+++ tests/feedback.test.js
@@ -1,7 +1,16 @@
 import { Selector, t } from 'testcafe'
+import FeedbackPage from '../page-objects/pages/FeedbackPage'
+const feedbackPage = new FeedbackPage()
 
 fixture`Getting started with TestCafe`.page(
 	'https://devexpress.github.io/testcafe/example/'
 )
 
-test.only('My First testcafe test', async (t) => {})
+test.only('My First testcafe test', async (t) => {
+	// Selector
+	// Actions
+	feedbackPage.setDeveloperName()
+	feedbackPage.getArticleText()
+	// assertion
+	expect(feedbackPage.getArticleText().contains('Jhon'))
+})
